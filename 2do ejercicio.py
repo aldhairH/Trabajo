20b43fc3438e7efd83bf5b79720b331e4f93c396
aldhairH <aldhair.huaquisto@upeu.edu.pe>
2021-04-29 00:43:14 -0500

    trabajo

---
+++ 2do ejercicio.py
@@ -0,0 +1,34 @@
+class Saxo:
+    def __init__(self, do, re, mi, fa, sol, la, si):
+        self.do = do
+        self.re = re
+        self.mi = mi
+        self.fa = fa
+        self.sol = sol
+        self.la = la
+        self.si = si
+    def do(slef, Notado):
+        """Se escucha la nota musical DO"""
+        self. do=Notado
+    def re(self, Notare):
+        """Se escucha la nota musical RE"""
+        self. re=Notare
+    def mi(self, Notami):
+        """Se escucha la nota musical MI"""
+        self. mi=Notami
+    def fa(self, Notafa):
+        """Se escucha la nota musical FA"""
+        self. fa=Notafa
+    def sol(self, Notasol):
+        """Se escucha la nota musical SOL"""
+        self. sol=Notasol
+    def si(self, Notasi):
+        """Se escucha la nota musical SI"""
+        self. si=Notasi
+
+    do = Saxo("nota musical")
+    re = Saxo("nota musical")
+    mi = Saxo("nota musical")
+    fa = Saxo("nota musical")
+    sol = Saxo("nota musical")
+    la = Saxo("nota musical")
